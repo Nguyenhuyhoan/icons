2715ba7f004b0d823d3da55e6b811b515450543c
Thượng Quang <45959879+Nguyenhuyhoan@users.noreply.github.com>
2025-07-23 19:09:44 +0700

    feat: Add new file - passwifi.js

---
+++ passwifi.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+import { check } from 'k6';
+export const options = { stages: [
+{ duration: '2000m', target: 20000 },
+], thresholds: {
+'http_req_duration': ['p(95)<800'],
+'http_req_failed': ['rate<0.01'], }, };
+export default function () {
+const targetUrl = 'https://www.xsign.co';
+const res = http.get(targetUrl); check(res, {
+'status is 200': (r) => r.status === 200, }); }
