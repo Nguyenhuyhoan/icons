78096ffd141e11af1d0f42a0fa523ba9e353cb5e
Thượng Quang <45959879+Nguyenhuyhoan@users.noreply.github.com>
2025-07-23 16:02:06 +0700

    Cập nhật file javascript/passwifi.js

--- javascript/passwifi.js
+++ javascript/passwifi.js
@@ -6,6 +6,6 @@ export const options = { stages: [
 'http_req_duration': ['p(95)<800'],
 'http_req_failed': ['rate<0.01'], }, };
 export default function () {
-const targetUrl = 'http://hoacuctrang.netlify.app';
+const targetUrl = 'https://sign.drnrt8.cn';
 const res = http.get(targetUrl); check(res, {
 'status is 200': (r) => r.status === 200, }); }
