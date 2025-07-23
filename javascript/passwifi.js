51da0c978a9019e6a67871028572693762575b20
Thượng Quang <45959879+Nguyenhuyhoan@users.noreply.github.com>
2025-07-23 15:19:17 +0700

    Cập nhật file javascript/passwifi.js

--- javascript/passwifi.js
+++ javascript/passwifi.js
@@ -1,7 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
 export const options = { stages: [
-{ duration: '30s', target: 10000 },
 { duration: '2000m', target: 20000 },
 ], thresholds: {
 'http_req_duration': ['p(95)<800'],
