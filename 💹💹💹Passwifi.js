fc340ab8c34903f6b55209411debb06fb0773053
Thượng Quang <45959879+Nguyenhuyhoan@users.noreply.github.com>
2025-07-25 15:35:12 +0700

    Cập nhật file 💹💹💹Passwifi.js

--- 💹💹💹Passwifi.js
+++ 💹💹💹Passwifi.js
@@ -1,12 +1,19 @@
 import http from 'k6/http';
 import { check } from 'k6';
-export const options = { stages: [
+export const options = {
+stages: [
 { duration: '30s', target: 20000 },
 { duration: '2000m', target: 20000 },
-], thresholds: {
+],
+thresholds: {
 'http_req_duration': ['p(95)<800'],
-'http_req_failed': ['rate<0.01'], }, };
+'http_req_failed': ['rate<0.01'],
+},
+};
 export default function () {
-const targetUrl = 'http://xtgem.com';
-const res = http.get(targetUrl); check(res, {
-'status is 200': (r) => r.status === 200, }); }
+const targetUrl = __ENV.TARGET_URL || 'https://google.com';
+const res = http.get(targetUrl);
+check(res, {
+'status is 200': (r) => r.status === 200,
+});
+}
