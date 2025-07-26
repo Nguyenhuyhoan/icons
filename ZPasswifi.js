493981915fbb108d593612a2df8644384eaa7682
Thượng Quang <45959879+Nguyenhuyhoan@users.noreply.github.com>
2025-07-26 18:51:10 +0700

    Cập nhật file ZPasswifi.js

--- ZPasswifi.js
+++ ZPasswifi.js
@@ -1,7 +1,9 @@
 import http from 'k6/http';
 import { check } from 'k6';
+
 export const options = {
 stages: [
+
 { duration: '2s', target: 1000 },
 { duration: '5m', target: 1000 },
 { duration: '5m', target: 0 },
@@ -289,14 +291,19 @@ stages: [
 { duration: '2s', target: 1000 },
 { duration: '5m', target: 1000 },
 { duration: '5m', target: 0 },
-
 
 ],
 thresholds: {
 'http_req_duration': ['p(95)<800'],
 'http_req_failed': ['rate<0.01'],},};
+
 export default function () {
 const targetUrl = __ENV.TARGET_URL || 'https://google.com';
-const res = http.get(targetUrl);
+
+const headers = {
+'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 20_8 like Mac OS X) AppleWebKit/709.1.20 (KHTML, like Gecko) Version/20.8 Mobile/20A123 Safari/909.1',
+'Accept-Language': 'en-EN', 'Referer': targetUrl,};
+
+const res = http.get(targetUrl, { headers });
 check(res, {
 'status is 200': (r) => r.status === 200,});}
